b37cd75b334abd1803b877fcb6bb2942ccbfb3f1
vincent-0507 <engweixuan1@gmail.com>
2026-02-25 00:52:50 +0800

    Added book appointment

--- server/src/main/kotlin/com/group8/comp2300/data/repository/AppointmentRepository.kt
+++
@@ -1,7 +0,0 @@
-package com.group8.comp2300.data.repository
-
-import com.group8.comp2300.database.Appointment
-
-interface AppointmentRepository {
-    fun insertAppointment(appointment: Appointment)
-}
--- server/src/main/kotlin/com/group8/comp2300/data/repository/SlotRepository.kt
+++
@@ -1,8 +0,0 @@
-package com.group8.comp2300.data.repository
-
-import com.group8.comp2300.database.Slot
-
-interface SlotRepository {
-    fun getSlotById(slotId: String): Slot?
-    fun updateSlotBookingStatus(slotId: String, status: Long)
-}
