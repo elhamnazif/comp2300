735f2ebbb428b492e1f6aaada9105d940915c46c
eliana <eliana100317@gmail.com>
2026-02-25 12:11:15 +0800

    Represents calculated trends on a monthly basis.
    Note: DB missing for daily/monthly summary

---
+++ shared/src/commonMain/kotlin/com/group8/comp2300/domain/model/mood/MoodSummary.kt
@@ -0,0 +1,9 @@
+package com.group8.comp2300.domain.model.mood
+
+// represents calculated trend (monthly)
+
+data class MoodSummary (
+    val moodType: String,
+    val count: Int,
+    val percentage: Float
+)
