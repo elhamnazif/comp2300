716cf87bd8dd1a4ffc40317a05b844c594c72e93
vincent-0507 <engweixuan1@gmail.com>
2026-02-25 12:35:15 +0800

    update

---
+++ server/src/main/kotlin/com/group8/comp2300/database/SlotRepository.kt
@@ -0,0 +1,8 @@
+package com.group8.comp2300.database
+
+import com.group8.comp2300.database.AppointmentSlots
+
+interface SlotRepository {
+    fun getSlotById(id: String): AppointmentSlots?
+    fun updateSlotBookingStatus(id: String, isBooked: Long)
+}
